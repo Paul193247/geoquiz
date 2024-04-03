b177fa41dfa18c899bb604b9b0e0cd7e03051c8f
Paul Zörb <paulzoerb09@icloud.com>
2024-04-03 16:35:28 +0200

    content.json

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const text_div = document.getElementById("text");
 const options_div = document.getElementById("options");
 
-const path = fetch("content.json");
+const path = fetch("output.json");
 
 function getOnclick(path) {
   return () => {
